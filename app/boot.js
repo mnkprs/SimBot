f698a4947d27befde5b008793c442381b3a08515
Manos Kaparos <it21324@hua.gr>
2018-06-30 05:56:25 +0300

    redirect successfull

--- app/boot.js
+++ app/boot.js
@@ -5,67 +5,70 @@ var version = require('./package.json').version
 var EventEmitter = require('events')
 
 module.exports = function (cb) {
-  var zenbot = { version }
-  var args = minimist(process.argv.slice(3))
-  var conf = {}
-  var config = {}
-  var overrides = {}
-    console.log(args)
-  // 1. load conf overrides file if present
-  if(!_.isUndefined(args.conf)){
+    var zenbot = {version}
+    var args = minimist(['binance.ETH-BTC'])
+    var conf = {}
+    var config = {}
+    var overrides = {}
+
+    // 1. load conf overrides file if present
+    if (!_.isUndefined(args.conf)) {
+        try {
+            overrides = require(path.resolve(process.cwd(), args.conf))
+        } catch (err) {
+            console.error(err + ', failed to load conf overrides file!')
+        }
+    }
+
+    // 2. load conf.js if present
     try {
-      overrides = require(path.resolve(process.cwd(), args.conf))
+        conf = require('./conf')
     } catch (err) {
-      console.error(err + ', failed to load conf overrides file!')
+        console.error(err + ', falling back to conf-sample')
     }
-  }
 
-  // 2. load conf.js if present
-  try {
-    conf = require('./conf')
-  } catch (err) {
-    console.error(err + ', falling back to conf-sample')
-  }
+    // 3. Load conf-sample.js and merge
+    var defaults = require('./conf-sample')
+    _.defaultsDeep(config, overrides, conf, defaults)
+    zenbot.conf = config
 
-  // 3. Load conf-sample.js and merge
-  var defaults = require('./conf-sample')
-  _.defaultsDeep(config, overrides, conf, defaults)
-  zenbot.conf = config
+    var eventBus = new EventEmitter()
+    zenbot.conf.eventBus = eventBus
 
-  var eventBus = new EventEmitter()
-  zenbot.conf.eventBus = eventBus
+    var authStr = '', authMechanism, connectionString
 
-  var authStr = '', authMechanism, connectionString
+    if (zenbot.conf.mongo.username) {
+        authStr = encodeURIComponent(zenbot.conf.mongo.username)
 
-  if(zenbot.conf.mongo.username){
-    authStr = encodeURIComponent(zenbot.conf.mongo.username)
+        if (zenbot.conf.mongo.password) authStr += ':' + encodeURIComponent(zenbot.conf.mongo.password)
 
-    if(zenbot.conf.mongo.password) authStr += ':' + encodeURIComponent(zenbot.conf.mongo.password)
+        authStr += '@'
 
-    authStr += '@'
-
-    // authMechanism could be a conf.js parameter to support more mongodb authentication methods
-    authMechanism = zenbot.conf.mongo.authMechanism || 'DEFAULT'
-  }
-
-  if (zenbot.conf.mongo.connectionString) {
-    connectionString = zenbot.conf.mongo.connectionString
-  } else {
-    connectionString = 'mongodb://' + authStr + zenbot.conf.mongo.host + ':' + zenbot.conf.mongo.port + '/' + zenbot.conf.mongo.db + '?' +
-      (zenbot.conf.mongo.replicaSet ? '&replicaSet=' + zenbot.conf.mongo.replicaSet : '' ) +
-      (authMechanism ? '&authMechanism=' + authMechanism : '' )
-  }
-
-  require('mongodb').MongoClient.connect(connectionString, function (err, client) {
-    if (err) {
-      console.error('WARNING: MongoDB Connection Error: ', err)
-      console.error('WARNING: without MongoDB some features (such as backfilling/simulation) may be disabled.')
-      console.error('Attempted authentication string: ' + connectionString)
-      cb(null, zenbot)
-      return
+        // authMechanism could be a conf.js parameter to support more mongodb authentication methods
+        authMechanism = zenbot.conf.mongo.authMechanism || 'DEFAULT'
     }
-    var db = client.db(zenbot.conf.mongo.db)
-      _.set(zenbot, 'conf.db.mongo', db)
-    cb(null, zenbot)
-  })
+
+    if (zenbot.conf.mongo.connectionString) {
+        connectionString = zenbot.conf.mongo.connectionString
+    } else {
+        connectionString = 'mongodb://' + authStr + zenbot.conf.mongo.host + ':' + zenbot.conf.mongo.port + '/' + zenbot.conf.mongo.db + '?' +
+            (zenbot.conf.mongo.replicaSet ? '&replicaSet=' + zenbot.conf.mongo.replicaSet : '') +
+            (authMechanism ? '&authMechanism=' + authMechanism : '')
+    }
+    return new Promise(resolve => {
+        require('mongodb').MongoClient.connect(connectionString, function (err, client) {
+            if (err) {
+                console.error('WARNING: MongoDB Connection Error: ', err)
+                console.error('WARNING: without MongoDB some features (such as backfilling/simulation) may be disabled.')
+                console.error('Attempted authentication string: ' + connectionString)
+            }
+
+            var db = client.db(zenbot.conf.mongo.db)
+            _.set(zenbot, 'conf.db.mongo', db)
+            cb(null, zenbot).then((b) => resolve(b))
+        })
+    })
+
+
 }
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var files = require('../public/javascripts/fileRead');
+var path = require('path')
+var fs = require('fs')
+var boot = require('../boot')
+var sim = require('../commands/sim')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'})
+
 });
 
-router.get('/test', function(req, res, next) {
-    res.render('test', { files : files });
+router.post('/', function (req, res, next) {
+    boot(function (err, zenbot) {
+        return new Promise(resolve => {
+            sim(zenbot.conf).then((a) => {
+                resolve(a)
+            }).catch((err) => {
+                console.error(err)
+            })
+        })
+    }).then((a)=> {
+        console.log(a)
+        res.redirect('/'+ a)
+    }).catch((err) => {
+        console.error(err)
+    })
 });
 
-router.get('/:uniqueHtmlFileName', function(req , res){
-    res.sendfile('/home/osboxes/code/thesis/ThesisApp/app/simulations/' + req.params.uniqueHtmlFileName);
+router.get('/test', function (req, res, next) {
+    res.render('test', {files: files});
+});
+
+router.get('/:uniqueHtmlFileName', function (req, res) {
+    res.sendFile('/home/osboxes/code/thesis/ThesisApp/app/simulations/' + req.params.uniqueHtmlFileName + '.html');
 });
 
 module.exports = router;
