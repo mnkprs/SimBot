460065b08f4ac04e5bca5511f5c9805ac56bb956
Manos Kaparos <it21324@hua.gr>
2018-08-04 22:29:38 +0300

    Revert "js for getting all strategies"
    This reverts commit 9d22e590f99d39a79f2ce5b541e2a012e92840f1.

--- app/public/javascripts/strategyData.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-var strategies = fs.readdirSync('extensions/strategies')
-var strategyData = []
-strategies.forEach((strategy) => {
-    let strat = require(`../../extensions/strategies/${strategy}/strategy`)
-    strategyData.push(strat)
-})
-
-module.exports = strategyData
