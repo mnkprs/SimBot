7dc4b4e8be822d33b94157bbc55dc073900fb677
mnkprs <mnkzyy@hotmail.com>
2019-06-25 21:59:46 +0300

    moved server starting configuration to app.js

--- app/app.js
+++ app/app.js
@@ -8,37 +8,120 @@ var indexRouter = require('./routes/index');
 var backfillRouter = require('./routes/backfill');
 var usersRouter = require('./routes/users');
 const bodyParser = require('body-parser');
-var app = express();
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+var http = require('http');
+var debug = require('debug')('app:server');
 
+
+if (cluster.isMaster) {
+
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork(); // Create a New Worker, If Worker is Dead
+    });
+
+
+} else {
+
+    var app = express();
+    var port = normalizePort(process.env.PORT || '3000');
+
+
+    var server = http.createServer(app);
+    app.set('port', port);
+    if(server.address() === null) {
+        server.listen(port);
+    }
+
+    server.on('error', onError);
+    server.on('listening', onListening);
+
+
+    function normalizePort(val) {
+        var port = parseInt(val, 10);
+
+        if (isNaN(port)) {
+            // named pipe
+            return val;
+        }
+
+        if (port >= 0) {
+            // port number
+            return port;
+        }
+
+        return false;
+    }
+    function onError(error) {
+        if (error.syscall !== 'listen') {
+            throw error;
+        }
+
+        var bind = typeof port === 'string'
+            ? 'Pipe ' + port
+            : 'Port ' + port;
+
+        // handle specific listen errors with friendly messages
+        switch (error.code) {
+            case 'EACCES':
+                console.error(bind + ' requires elevated privileges');
+                process.exit(1);
+                break;
+            case 'EADDRINUSE':
+                console.error(bind + ' is already in use');
+                process.exit(1);
+                break;
+            default:
+                throw error;
+        }
+    }
+    function onListening() {
+        var addr = server.address();
+        var bind = typeof addr === 'string'
+            ? 'pipe ' + addr
+            : 'port ' + addr.port;
+        debug('Listening on ' + bind);
+
+    }
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials')
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'hbs');
+    hbs.registerPartials(__dirname + '/views/partials')
+    //app.set('port', '3000');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', indexRouter);
-app.use('/backfill', backfillRouter);
-app.use('/users', usersRouter);
+    app.use('/', indexRouter);
+    app.use('/backfill', backfillRouter);
+    app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        next(createError(404));
+    });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // error handler
+    app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+}
 module.exports = app;
